c1fc5dcb13ea3e62b71591b1d2419bc0809d9d4e
Michael Himes <mhimes@knights.ucf.edu>
2020-07-14 13:16:25 -0400

    Updated ilog/olog to allow single element

--- MARGE.py
+++ MARGE.py
@@ -126,6 +126,8 @@ def MARGE(confile):
                 ilog = conf.getboolean("ilog")
             elif conf["ilog"] in ["None", "none", ""]:
                 ilog = False
+            elif conf["ilog"].isdigit():
+                ilog = int(conf["ilog"])
             elif any(pun in conf["ilog"] for pun in [",", " ", "\n"]):
                 if "," in conf["ilog"]:
                     ilog = [int(num) for num in conf["ilog"].split(',')]
@@ -141,6 +143,8 @@ def MARGE(confile):
                 olog = conf.getboolean("olog")
             elif conf["olog"] in ["None", "none", ""]:
                 olog = False
+            elif conf["olog"].isdigit():
+                olog = int(conf["olog"])
             elif any(pun in conf["olog"] for pun in [",", " ", "\n"]):
                 if "," in conf["olog"]:
                     olog = [int(num) for num in conf["olog"].split(',')]
